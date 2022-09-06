a98d7b3ea7b4d54ab6bb8dec2993035123933a7c
CNordqvist <christoffer.nord93@gmail.com>
2022-09-06 19:47:34 +0200

    Inga färger, ingen meny.

---
+++ StringPainter/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string line = "29535123p48723487597645723645";
+
+
+for (int i = 0; i < line.Length - 1; i++)
+{
+
+    if (!char.IsDigit(line[i])) //Kolla om i är en siffra annars hoppa raden
+    {
+        continue;
+    }
+
+    for (int j = i + 1; j < line.Length; j++) // Hitta sista siffran i giltiga tal
+    {
+        if (!char.IsDigit(line[j]) )
+        {
+            break;
+        } 
+        else if (line[i] == line[j]) //När giltigt tal hittats skriv ut hela raden med talet markerat.
+        {
+            for (int k = 0; k < line.Length; k++)
+            {
+                if (k >= i && k <= j)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(line[k]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(line[k]);
+                }
+            }
+            Console.WriteLine("");
+            break;
+        }
+    }
+}
+
+Console.ReadKey();
