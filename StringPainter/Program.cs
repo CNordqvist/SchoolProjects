fe08a287ac068bc6ae4fc1f3bcd99e4cac091624
CNordqvist <christoffer.nord93@gmail.com>
2022-09-07 22:23:58 +0200

    colorSwitch

--- StringPainter/Program.cs
+++ StringPainter/Program.cs
@@ -1,42 +1,112 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+while (true) {
+  
+    Random random = new Random();
+    string line = "29535123p48723487597645723645";
+    char input;
 
-string line = "29535123p48723487597645723645";
+    //användar meny
 
+    Console.WriteLine("\nVill du göra en egen string? y/n. s för stop");
+    char.TryParse(Console.ReadLine(), out input);
 
-for (int i = 0; i < line.Length - 1; i++)
-{
-
-    if (!char.IsDigit(line[i])) //Kolla om i är en siffra annars hoppa raden
+    if (input == 'y')
     {
+        Console.WriteLine("Var god och skriv in den sträng du vill använda");
+        line = Console.ReadLine();
+    } 
+    else if (input == 's')
+    {
+        break;
+    }
+    else if (input != 'n') 
+    {
+        Console.WriteLine("Unrecognized character");
         continue;
     }
 
-    for (int j = i + 1; j < line.Length; j++) // Hitta sista siffran i giltiga tal
+
+    for (int i = 0; i < line.Length-1; i++)
     {
-        if (!char.IsDigit(line[j]) )
+        int color = random.Next(4);
+
+        if (!char.IsDigit(line[i]))
         {
-            break;
-        } 
-        else if (line[i] == line[j]) //När giltigt tal hittats skriv ut hela raden med talet markerat.
+            continue;
+        }
+
+        for (int j = i+1; j < line.Length; j++)
         {
-            for (int k = 0; k < line.Length; k++)
+            if (!char.IsDigit(line[j]))
             {
-                if (k >= i && k <= j)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write(line[k]);
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.Write(line[k]);
-                }
+                break;
             }
-            Console.WriteLine("");
-            break;
+
+            //hitta sista siffran i ett tal som stämmer med kriterierna, printa linjen
+            else if (line[j] == line[i]) 
+                
+            {
+                for (int l = 0; l < line.Length; l++)
+                {
+                    if (l >= i && l <= j)
+                    {
+                        switch (color) {
+                            case 0:
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                break;
+                            case 1:
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                break;
+                            case 2:
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                break;
+                            case 3:
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                break;
+                        }
+                        Console.Write(line[l]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(line[l]);
+                    }
+                    
+                }
+                Console.WriteLine("");
+                break;
+            }
         }
     }
 }
 
-Console.ReadKey();
+// 
+/*
+// be om text #1
+Console.WriteLine("Please write the first string");
+string userString1 = Console.ReadLine();
+// be om text #2
+Console.WriteLine("Please write the string inside the first string you want found");
+string userString2 = Console.ReadLine();
+
+
+
+// hitta text2 innuti text1
+int index1 = userString1.IndexOf(userString2);
+
+// skriv ut med text 2 markerad
+for (int i = 0; i < userString1.Length; i++)
+{
+    if (i >= index1 && i < index1+ userString2.Length)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(userString1[i]);
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.Write(userString1[i]);
+    }
+}
+*/
